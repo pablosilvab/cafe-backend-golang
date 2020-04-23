691283e187a19349143f25d0f1add78f5aa0246b
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-22 23:32:35 -0400

    add config file -  this file contains configurations properties

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,32 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/tkanos/gonfig"
 )
+
+const defaultConfigPath = "./config/config.json"
+
+type Configuration struct {
+	Port string
+}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Link!")
 }
 
 func main() {
+
+	// Configuration
+	configuration := Configuration{}
+	err := gonfig.GetConf(defaultConfigPath, &configuration)
+	if err != nil {
+		log.Printf("Error: %s", err.Error())
+		return
+	}
+
+	// Routing
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Running on port " + configuration.Port)
+	log.Fatal(http.ListenAndServe(":"+configuration.Port, router))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/pablosilvab/cafe-backend-golang
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4 // indirect
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/spf13/viper v1.6.3 // indirect
+	github.com/tkanos/gonfig v0.0.0-20181112185242-896f3d81fadf // indirect
+)
