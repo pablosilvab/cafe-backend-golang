c1cb7faec542682bf2e81a4ab5ea934af6e56ee7
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-22 22:27:19 -0400

    add server and update gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Link!")
+}
 
 func main() {
-	fmt.Print("Hello World")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
