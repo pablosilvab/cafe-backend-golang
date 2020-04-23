35706c4a94d3c311580ab16aa724835910398d33
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-22 23:37:06 -0400

    add mongodb connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/tkanos/gonfig"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const defaultConfigPath = "./config/config.json"
@@ -20,10 +23,29 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// MongoDB
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 
 	// Configuration
 	configuration := Configuration{}
-	err := gonfig.GetConf(defaultConfigPath, &configuration)
+	err = gonfig.GetConf(defaultConfigPath, &configuration)
 	if err != nil {
 		log.Printf("Error: %s", err.Error())
 		return
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/spf13/viper v1.6.3 // indirect
 	github.com/tkanos/gonfig v0.0.0-20181112185242-896f3d81fadf // indirect
+	go.mongodb.org/mongo-driver v1.3.2 // indirect
 )
