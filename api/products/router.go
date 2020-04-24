44ecd117af46e5687bbb2a6ff848da040af342db
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-24 00:35:07 -0400

    test go report

--- api/products/router.go
+++ api/products/router.go
@@ -8,7 +8,19 @@ import (
 
 	"github.com/pablosilvab/cafe-backend-golang/api"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type Product struct {
+	//Id          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Available   bool               `json:"disponible,omitempty" bson:"disponible,omitempty"`
+	Name        string             `json:"nombre,omitempty" bson:"nombre,omitempty"`
+	Description string             `json:"descripcion,omitempty" bson:"descripcion,omitempty"`
+	Price       int32              `json:"precioUni,omitempty" bson:"precioUni,omitempty"`
+	Category    primitive.ObjectID `json:"categoria,omitempty" bson:"categoria,omitempty"`
+	User        primitive.ObjectID `json:"usuario,omitempty" bson:"usuario,omitempty"`
+	Image       string             `json:"img,omitempty" bson:"img,omitempty"`
+}
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 
